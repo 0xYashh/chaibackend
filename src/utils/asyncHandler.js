cbf5a7aace3021a0760cc8300cd39d59073e4427
Yash Kumar <yashkr044@gmail.com>
2025-04-01 20:00:28 +0530

    setting up utils

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,38 @@
+// promises 
+
+const asyncHandler = (requestHandler) =>{
+    (req,res,next) => {
+        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+    }
+    
+
+}
+
+export {asyncHandler}
+
+
+//try - catch
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => {()=>{}}
+// consr asyncHandler = (func) => async ()=>{}
+
+
+
+// const asyncHandler = (fn) =>{ async (req,res,next) => {
+//     try{
+//         await fn(req,res,next)
+
+//     } catch (error){
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message: err.message
+//         })
+//     }
+// }
+
+// }
+
+
+
+
+//asyncHandler method
