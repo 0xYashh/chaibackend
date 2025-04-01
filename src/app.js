a6a1e25958c4d5f19eb71878a986dd46a35de16b
Yash Kumar <yashkr044@gmail.com>
2025-04-02 02:23:31 +0530

    setting up controllers,middlerwares and routes

--- src/app.js
+++ src/app.js
@@ -20,4 +20,20 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
+
+
+//routes import
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+// app.use("/users",userRouter)
+app.use("/api/v1/users",userRouter)
+
+// http://localhost:8000/user/register
+// http://localhost:8000/api/v1/user/register
+
+// http://localhost:8000/user/login
+
 export {app}
